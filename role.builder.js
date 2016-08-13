fe58f3fffd8502c7d19fb7f24bffca1e8c26bf73
L00Cyph3r <l00cyph3r@l00cyph3r.com>
2016-08-13 19:29:13 +0200

    Added check for creep.spawning

--- role.builder.js
+++ role.builder.js
@@ -2,26 +2,28 @@ var roleUpgrader = require('role.upgrader');
 
 module.exports = {
   run: function(creep) {
-    if (creep.memory.working === true) {
-      if (creep.carry.energy === 0) {
-        creep.memory.working = false;
-      } else {
-        var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-        if (constructionSite !== undefined) {
-          if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(constructionSite);
-          }
+    if (creep.spawning === false) {
+      if (creep.memory.working === true) {
+        if (creep.carry.energy === 0) {
+          creep.memory.working = false;
         } else {
-          roleUpgrader.run(creep);
+          var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+          if (constructionSite !== undefined) {
+            if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
+              creep.moveTo(constructionSite);
+            }
+          } else {
+            roleUpgrader.run(creep);
+          }
         }
-      }
-    } else {
-      if (creep.carry.energy === creep.carryCapacity) {
-        creep.memory.working = true;
-      }
-      var source = creep.pos.findClosestByPath(FIND_SOURCES);
-      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(source);
+      } else {
+        if (creep.carry.energy === creep.carryCapacity) {
+          creep.memory.working = true;
+        }
+        var source = creep.pos.findClosestByPath(FIND_SOURCES);
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
       }
     }
   }
--- role.wallRepairer.js
+++ role.wallRepairer.js
@@ -2,36 +2,38 @@ var roleBuilder = require('role.builder');
 
 module.exports = {
   run: function(creep) {
-    if (creep.memory.working === true) {
-      if (creep.carry.energy === 0) {
-        creep.memory.working = false;
-      }
-      var target = undefined;
-      for (let percentage = 0.1; percentage <= 1; percentage = percentage + 0.1) {
-        target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-          filter: (s) => s.structureType === STRUCTURE_WALL &&
-            s.hits / s.hitsMax < percentage
-        });
-        if (target !== undefined) {
-          break;
+    if (creep.spawning === false) {
+      if (creep.memory.working === true) {
+        if (creep.carry.energy === 0) {
+          creep.memory.working = false;
         }
-      }
-      if (target != undefined) {
-        if (creep.repair(target) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
+        var target = undefined;
+        for (let percentage = 0.1; percentage <= 1; percentage = percentage + 0.1) {
+          target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (s) => s.structureType === STRUCTURE_WALL &&
+              s.hits / s.hitsMax < percentage
+          });
+          if (target !== undefined) {
+            break;
+          }
+        }
+        if (target != undefined) {
+          if (creep.repair(target) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+          }
+        } else {
+          roleBuilder.run(creep);
         }
       } else {
-        roleBuilder.run(creep);
-      }
-    } else {
-      if (creep.carry.energy === creep.carryCapacity) {
-        creep.memory.working = true;
-      }
-      var source = creep.pos.findClosestByPath(FIND_SOURCES, {
-        filter: (s) => s.energy > 0
-      });
-      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(source);
+        if (creep.carry.energy === creep.carryCapacity) {
+          creep.memory.working = true;
+        }
+        var source = creep.pos.findClosestByPath(FIND_SOURCES, {
+          filter: (s) => s.energy > 0
+        });
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
       }
     }
   }

8fb73e9fb7795c4a11db8fdd9dad1550d7547454
L00Cyph3r <l00cyph3r@l00cyph3r.com>
2016-08-13 14:02:50 +0200

    Update snippet to reflect changes in prototype.spawn.js

--- snippets.js
+++ snippets.js
@@ -1,4 +1,4 @@
-Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {
+Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], "H-" + Math.floor((Math.random() * 1000) + 1), {
   working: false,
   role: 'harvester'
 });

ab644bddc6f96473b2b8cdb9cb13af89acf46e08
L00Cyph3r <l00cyph3r@l00cyph3r.com>
2016-08-15 07:18:52 +0200

    defence kinda working? Still need to get some hostiles to test

---
+++ functions.js
@@ -0,0 +1,4 @@
+var funcDefence = require("func.defence");
+module.exports = {
+  defence: funcDefence
+};
---
+++ role.attacker.js
@@ -0,0 +1,36 @@
+module.exports = {
+  run: function(creep) {
+    if (creep.spawning === false) {
+      if (creep.memory.mode === 'defending') {
+        var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1);
+        if (targets.length > 0) {
+          creep.attack(targets[0]);
+        } else {
+          var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+          if (targets.length > 0) {
+            creep.rangedAttack(targets[0]);
+          } else {
+            var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS);
+            if (targets.length > 0) {
+              creep.moveTo(targets[0]);
+            }
+          }
+        }
+      } else if (creep.memory.mode === 'attacking') {
+        var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1);
+        if (targets.length > 0) {
+          creep.attack(targets[0]);
+        } else {
+          var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+          if (targets.length > 0) {
+            creep.rangedAttack(targets[0]);
+          }
+        }
+        var targets = creep.pos.findInRange(FIND_HOSTILE_SPAWNS, 3);
+        if (targets.length > 0) {
+          creep.attack(targets[0]);
+        }
+      }
+    }
+  }
+};

e3efb2da4bc7c650677e518a620050c559db8094
L00Cyph3r <l00cyph3r@l00cyph3r.com>
2016-11-23 21:27:30 +0100

    Set rally position on Game.time % 10

--- role.attacker.js
+++ role.attacker.js
@@ -1,5 +1,9 @@
 module.exports = {
   run: function(creep) {
+    if (Game.time % 10 === 0) {
+      creep.memory.targetLocation[0] = Game.flags['Rally1'].pos.x;
+      creep.memory.targetLocation[1] = Game.flags['Rally1'].pos.y;
+    }
     if (creep.spawning === false) {
       if (creep.memory.mode === 'defending') {
         var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1);
